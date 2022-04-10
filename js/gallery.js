06982cd6837896a50d2ebf1ffa0e13777eb7d79c
Srijan Timsina <81183382+SrijanTimsina@users.noreply.github.com>
2022-04-10 10:39:27 +0545

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -12,8 +12,8 @@ function imgClick(e) {
 		return;
 	} else {
 		img.forEach((img) => (img.style.opacity = 1));
-		current.src = e.target.src;
 		fullScreenImage.setAttribute("href", e.target.src);
+		current.src = e.target.src;
 		current.classList.add("fade-in");
 		setTimeout(() => current.classList.remove("fade-in"), 500);
 		e.target.style.opacity = opacity;
